4af5ed00108c18f54e590f303a327c710ee3fdec
“ujjwal <ujjwalrsvm@gmail.com>
2025-04-29 14:17:59 +0530

    added new file

---
+++ bookstore/books/models.py
@@ -0,0 +1,13 @@
+
+# Create your models here.
+from django.db import models 
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
