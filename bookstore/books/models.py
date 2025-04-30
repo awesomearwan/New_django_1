31cbd07f6182453292b3c26aa47633a52ad124e2
“ujjwal <ujjwalrsvm@gmail.com>
2025-04-30 13:54:02 +0530

    added new file

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,6 +1,4 @@
-
-# Create your models here.
-from django.db import models 
+from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +9,12 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-# Create your views here.
-from django.views.generic import ListView, DetailView
-from .models import Book
+from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import redirect
+from .models import Book, Contact
+
+class HomeView(TemplateView):
+    template_name = 'books/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['featured_books'] = Book.objects.filter(is_visible=True)[:3]
+        return context
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        Contact.objects.create(name=name, email=email, message=message)
+        return redirect('books:home')
 
 class BookListView(ListView):
     model = Book
@@ -18,4 +32,3 @@ class BookDetailView(DetailView):
 
     def get_queryset(self):
         return Book.objects.filter(is_visible=True)
-
